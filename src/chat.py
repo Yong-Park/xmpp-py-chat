7dc1b1fce339bf892c7862c1e79c26c1f1db2114
Santiago Taracena <tar20017@uvg.edu.gt>
2023-08-16 14:54:23 -0600

    Commenting chat.py

--- src/chat.py
+++ src/chat.py
@@ -5,20 +5,27 @@ Santiago Taracena Puga (20017)
 Proyecto 1 - Protocolo XMPP
 """
 
+# Useful utilities for the chat file.
 from utils.client import Client
 from utils.register_new_user import register_new_user
 
+# If name equals main (good practice).
 if (__name__ == "__main__"):
 
+    # Chat's title.
     print("\n- * - XMPP Chat - * -\n")
 
+    # Instantiating a selected option.
     selected_option = str()
 
+    # While loop to display the initial menu.
     while (selected_option != "3"):
 
+        # Initial menu and selected option input.
         print("Main Options:\n\t1. Sign up to XMPP Chat.\n\t2. Sign in to XMPP Chat.\n\t3. Close XMPP Chat.\n\t4. Delete account on XMPP Chat.\n")
         selected_option = input("Please input the option you want to execute: ")
 
+        # First case, to sign up into the class' server.
         if (selected_option == "1"):
             jid = input("\nInput your new JID please: ")
             password = input("Now please input your new password: ")
@@ -26,6 +33,7 @@ if (__name__ == "__main__"):
             status_message = "\nYour sign up has been succesful!\n" if (status) else "\nYour sign up has thrown an error...\n"
             print(status_message)
 
+        # Second case, to sign in into the class' server.
         if (selected_option == "2"):
             jid = input("\nInput your JID please: ")
             password = input("Now please input your password: ")
