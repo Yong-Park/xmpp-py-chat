d62564e1d7bb571f732fd4d7ea3b7cdd6e511c14
Santiago Taracena <tar20017@uvg.edu.gt>
2023-08-15 22:52:37 -0600

    Finished showing a contac's info

--- src/utils/client.py
+++ src/utils/client.py
@@ -32,7 +32,7 @@ class Client(slixmpp.ClientXMPP):
         while (self.is_user_connected):
 
             # Client's menu and option input.
-            print("Chat Options:\n\t1. Show all my contacts.\n\t9. Sign out.\n")
+            print("Chat Options:\n\t1. Show all my contacts.\n\t2. Show a contact info.\n\t3. Send contact request.\n\t9. Sign out.\n")
             selected_option = input("Please input the option you want to execute: ")
 
             # Option to show all contacts.
@@ -70,3 +70,58 @@ class Client(slixmpp.ClientXMPP):
                 # Show contact's status.
                 status = presence["status"] or "None"
                 print(f"Contact status: {status}")
+
+    # Async function to show a contact by JID.
+    async def show_contact_info(self):
+
+        # Input to get the JID to request.
+        contact_jid = input("Input the contact's JID please: ")
+
+        # Client roster containing the contacts.
+        client_roster = self.client_roster
+
+        # Contact has not been found.
+        found = False
+
+        # Returning nothing if no contacts found.
+        if (not client_roster):
+            print("\nNo contacts found.")
+            return
+
+        # Iterating through the contacts.
+        for contact in client_roster.keys():
+
+            # If statement to continue if the contact finds itself.
+            if (contact == contact_jid):
+
+                # Contact has been found.
+                found = True
+
+                # Show contact's JID.
+                print(f"\nContact JID: {contact}")
+
+                # Iterating through the contact's information.
+                for _, presence in client_roster.presence(contact).items():
+
+                    # Show contact's presence.
+                    presence_value = presence["show"] or "Logged from Santiago's chat!"
+                    print(f"Contact presence: {presence_value}")
+
+                    # Show contact's status.
+                    status = presence["status"] or "None"
+                    print(f"Contact status: {status}")
+
+        # Text that shows if the contact was not found.
+        if (not found):
+            print("\nSeems like you haven't requested the user's contact...")
+
+    # Async function that sends a contact request to someone.
+    async def send_contact_request(self):
+
+        # Input to get the new contact's JID.
+        contact_jid = input("Input your new contact's JID please: ")
+
+        # Process to send a presence subscription.
+        self.send_presence_subscription(contact_jid)
+        print("Your request has been properly sent.")
+        await self.get_roster()
